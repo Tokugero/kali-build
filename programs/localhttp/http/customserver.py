834e6626ff3a50faaadcd4d0801b2aaa49123ef0
Philip Almueti <palmueti@gmail.com>
2025-02-17 16:16:08 -0800

    Add bloodhound and placeholder for localhttp

---
+++ programs/localhttp/http/customserver.py
@@ -0,0 +1,5 @@
+# TODO: Python script to start a generic server and parse out the following uris:
+# misc (should be output as a string)
+# miscb64 (should parse out the base64 and output as a string)
+# cookie (should parse out the cookie and output as a string)
+# cb64 (should parse out the base64 and output as a string)
